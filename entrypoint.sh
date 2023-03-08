a747d31b2939f2b35c8b8f1f8720103daf6e2fa8
pietrzakadrian <contact@pietrzakadrian.com>
2023-03-08 22:08:08 +0100

    feat(): clean code

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ set -e
 if [ -n "$1" ] && [ -n "$2" ]; then
    /composer/vendor/bin/ecs check $1 --clear-cache --config=$2
 else
-    echo "Missing required arguments: directory and config file path"
+    echo "Missing required arguments: directories and config file path"
 fi
