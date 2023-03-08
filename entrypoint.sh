67e3d591fa7756b88afb7311f0ee2927ea3ae49e
pietrzakadrian <contact@pietrzakadrian.com>
2023-03-08 21:59:03 +0100

    feat(): clean code

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-composer global require symplify/easy-coding-standard:^11.2 --prefer-dist --dev --no-progress
-
 if [ -n "$1" ] && [ -n "$2" ]; then
    /composer/vendor/bin/ecs check $1 --clear-cache --config=$2
 else
